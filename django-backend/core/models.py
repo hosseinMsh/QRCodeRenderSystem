ac670d3ae858c522b59dbb67fa17fb121253e20e
Hossein <hossein.masihi@gmail.com>
2025-10-05 11:59:14 +0330

    Add feature to display user notifications on the dashboard

--- django-backend/core/models.py
+++ django-backend/core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 class QRRender(models.Model):
-    id = models.UUIDField(primary_key=True,auto_created=True)
+    id = models.UUIDField(primary_key=True)
     # SHA256 of canonicalized request payload (the "signature" of options)
     options_hash = models.CharField(max_length=64, unique=True, db_index=True)
     # Original options JSON we sent to the Node service (normalized)
